1d2d5cbeb8b2188850d286bf398b83b26211b246
alisdemir33 <alisdemir33@gmail.com>
2021-06-23 07:49:42 +0300

    app used http hook

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -2,14 +2,28 @@ import { useState } from 'react';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import useHttp from  '../../hooks/httpHook';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+/*   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null); */
+  const [loading,error,data,sendRequest] = useHttp();
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
+   /*  setIsLoading(true);
+    setError(null); */
+    
+    sendRequest( 'https://react-http-asd-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
+    'POST',
+    JSON.stringify({ text: taskText }) 
+     )
+
+     const generatedId = data.name; // firebase-specific => "name" contains generated id
+     const createdTask = { id: generatedId, text: taskText };
+
+     props.onAddTask(createdTask);
+    
+    
     try {
       const response = await fetch(
         'https://react-http-asd-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
