770d098e6e90b21f0f83e9c1bc7a8f31f1badce3
alisdemir33 <alisdemir33@gmail.com>
2022-04-14 22:59:13 +0300

    cmt

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -6,25 +6,25 @@ import useHttp from '../../hooks/useHttp';
 
 const NewTask = (props) => {
 
-  const {isLoading,error,sendRequest} =useHttp();
+  const { isLoading, error, sendRequest } = useHttp();
 
-  const createTask = (taskText,taskData) =>{
-    const generatedId =taskData.id;
-    const cretaedTask = {id:generatedId, text:taskText}
-    props.onAddTask( cretaedTask);
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.id;
+    const cretaedTask = { id: generatedId, text: taskText }
+    props.onAddTask(cretaedTask);
   }
 
   const enterTaskHandler = async (taskText) => {
-   
+
     sendRequest(
-    {
-     url: 'https://react-http-asd-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
-      method: 'POST',    
-      headers: {
-        'Content-Type': 'application/json',
-      },      
-      body: { text: taskText },
-    }, createTask.bind(null,taskText) );
+      {
+        url: 'https://react-http-asd-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: { text: taskText },
+      }, createTask.bind(null, taskText));
   };
 
   return (
